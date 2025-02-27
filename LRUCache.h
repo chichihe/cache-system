7be8c6ff58c3cd4227b0b0b80985535a1db06616
chichihe <952495403@qq.com>
2025-02-27 23:48:41 +0800

    定义基类 添加LRU类

---
+++ LRUCache.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"CacheBase.h"
+template<typename Key, typename Value> class LRUCache;
+
+template<typename Key,typename Value> 
+class LRUNode : CacheBase {
+private:
+	Key _key;
+	Value _value;
+
+
+public:
+
+};
